9b294bd3b6f118cbc0148064ceed71499d073969
carlosbarnabe <carlosbarnabedev@gmail.com>
2022-08-08 14:35:27 -0300

    Arquivos para deploy no Heroku

--- pypro/settings.py
+++ pypro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%hkt$+b&xovhz%gzzw$*k-!f_@fsk4@pbphsn&41gew8ghs_is
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
